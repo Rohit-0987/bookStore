82478d4214e629262c43ac3fdce86fef353c4c7b
Rohit-0987 <99091666+Rohit-0987@users.noreply.github.com>
2024-08-17 16:16:14 +0530

    Add Bannner and footer in home page . Refactor Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,8 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Banner/>
+      <Footer/>
     </>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,21 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+  
 function Navbar() {
+
+  const [sticky,setSticky] = useState(false);
+  useEffect(()=>{
+    const handleScroll = ()=>{
+      if(window.scrollY>0){
+        setSticky(true);
+      }else{
+        setSticky(false);
+      }
+    }
+    window.addEventListener('scroll',handleScroll);
+    return ()=>{
+      window.removeEventListener('scroll',handleScroll);
+    }
+  },[])
   const navItem = (
     <>
       <li>
@@ -17,10 +32,11 @@ function Navbar() {
       </li>
     </>
   );
+
   return (
     <>
-      <div className="max-w--screen-2xl container mx-auto md:px-20 px-4">
-        <div className="navbar bg-base-100">
+      <div className={`max-w--screen-2xl container mx-auto md:px-20 px-4 fixed top-0 left-0 right-0 ${sticky?"sticky-navbar shadow-md bg-base-200 transition-all ease-in duration-300":""}`}>
+        <div className="navbar ">
           <div className="navbar-start">
             <div className="dropdown">
               <div
